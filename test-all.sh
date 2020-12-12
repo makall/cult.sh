4df0829d470fbe0401b01c6a1db6bc6d002c34dc
Fabio Mello <fmello@blupay.com.br>
2020-12-11 23:48:08 -0300

    cult should not override its caller script output.

--- test-all.sh
+++ test-all.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 # vim: ts=2 sw=2 sts=2 expandtab smartindent smarttab
 
-set -o errexit
-
-trap "./cult --report" EXIT
-
 cd "$(dirname "$0")" || exit
 
-./cult --begin --test-scenario "My Test Scenario"
+. ./cult
+
+./cult --scenario "My Test Scenario"
 
 ./test.sh 1
 ./test.sh 2
 ./test.sh 3
 ./test.sh 3
 
-./cult --test "My Test 4 A"
-./cult --test "My Test 4 A"
-./cult --test "My Test 4 A"
-./cult --test "My Test 4 A"
-./cult --test "My Test 4 A"
+./cult --step "My Test 4 A"
+./cult --step "My Test 4 A"
+./cult --step "My Test 4 A"
+./cult --step "My Test 4 A"
+./cult --step "My Test 4 A"
--- test.sh
+++ test.sh
@@ -1,27 +1,40 @@
 #!/usr/bin/env bash
 # vim: ts=2 sw=2 sts=2 expandtab smartindent smarttab
 
-set -o errexit
+TEST_COUNT="${1:-0}"
 
 cd "$(dirname "$0")" || exit
 
+if [ "$(ps -o stat= -p $$)" != "$(ps -o stat= -p $PPID)" ]; then
+	. ./cult
+fi
+
 ./cult \
-	--test-case "Test case $1" \
-	--test "My Test $1 B" \
-	--assert '.json.hello == "world"' \
-	--assert '.json.tester == "curl"' \
-	-a '.status == 200' \
+	--case "Test case $TEST_COUNT" \
+	--step "My Test $TEST_COUNT B" \
+	--test '.json.hello == "world"' \
+	--test '.json.tester == "curl"' \
+	--test '.status == 200' \
 	'http://echo.jsontest.com/hello/world/tester/curl'
 
-./cult --test "My Test $1 C" -v myIP '.json' --assert '.status == 200' 'http://ifconfig.me'
+./cult --step "My Test $TEST_COUNT C" -v myIP '.json' --assert '.status == 200' 'http://ifconfig.me'
 
-# shellcheck disable=SC2016
-./cult --test "My IP did not change" --assert '.json == $myIP' 'http://ifconfig.me'
+./cult --step "My IP did not change" --test ".json == \$myIP" 'http://ifconfig.me'
 
 ./cult --print '.'
 
-./cult -a '.status == 200' -a '.json.json.hello == $myIP' https://postman-echo.com/post <<- EOF
-{
-"hello": \$myIP
-}
+./cult -a '.status == 200' -a ".json.json.hello == \$myIP" https://postman-echo.com/post <<- EOF
+	{ "hello": \$myIP }
 EOF
+
+./cult --scenario 'My Scenario'
+echo "Some comment"
+
+echo "Some text on stderr" >&2
+
+echo "$blah"
+./cult --case 'My Case'
+echo "Another comment"
+
+./cult --step 'My Step'
+echo "Last comment"
